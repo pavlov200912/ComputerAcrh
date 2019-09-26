03fe5e01120983cdbc3f2b9f276cff4a33ae03b9
pavlov200912 <pavlov200912@gmail.com>
2019-09-26 23:51:40 +0300

    lab2 first try (worked)

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ all: main avrdude
 main: 
 	avr-gcc -g -O1 -std=c99 -mmcu=atmega328p -o out.elf hello.c
 avrdude:
-	sudo avrdude -v -patmega328p -P /dev/ttyACM0 -c arduino -U flash:w:"out.elf"
+	sudo avrdude -v -patmega328p -P /dev/ttyACM1 -c arduino -U flash:w:"out.elf"
--- hello.c
+++ hello.c
@@ -5,37 +5,56 @@
 #include <util/delay.h>
 
 
-int main() {
-  DDRB = 0x00;
-  PORTB = 0x01; 
-  DDRC = 0b00011111;   
-  int direction = 0; 
-  PORTC = 1 << 4;
+int button_is_pressed(int button_number) {
+  return !(PIND & (1 << button_number));
+}
 
-  while(1) {
-    if (PINB & 0x01) { 
-      // skip..
-    } else {
-      direction ^= 1;
-      PORTC = 0;
-      while (!(PINB & 0x01)) {}
-      PORTC = (0x01) | (1 << 4);
-      continue;
-    }
-    if (direction) {
-       if (PORTC & 1 << 4) {
-           PORTC = 0x01;
-       } else {
-           PORTC = (PORTC << 1); 
-       }
-    } else {
-      if (PORTC & (0x01)) {
-         PORTC = 1 << 4;
-      } else {
-         PORTC = (PORTC >> 1);
+int pressed_buttons_count() {
+  int count = 0;
+  for (int i = 0; i < 8; i++) {
+    count += button_is_pressed(i);
+  }
+  return count;
+}
+
+int get_pressed_button() {
+  int value = 0;
+  for (int i = 0; i < 8; i++) {
+    if (button_is_pressed(i)) 
+    { 
+      value = i; 
+      break;
+    } 
+  }
+  return value;
+}
+
+void display(int value) {
+   int i = 0;
+   while (value) {
+      if (value & 1) {
+        PORTB |= (1 << i);
       }
+      value /= 2;
+      i++;  
+   }
+}
+
+int main() {
+  DDRD = 0x00;
+  PORTD = 0xFF;
+  DDRB = 0xFF;
+  PORTB = 0x00;
+  
+  while (1) {
+    while (pressed_buttons_count() > 1) {}
+    if (pressed_buttons_count() == 1) {
+      PORTB = 0x00;
+      int value = get_pressed_button();
+      display(value);   
+      while (button_is_pressed(value)) {}
     }
-    _delay_ms(100);
+    _delay_ms(500);
   }
 
 }
